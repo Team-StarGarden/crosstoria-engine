14e70d26dd98619a911b27b54a119ea760628920
PieSignal <insiro@naver.com>
2020-01-15 00:33:46 +0900

    add : register api

--- src/routers/Users.ts
+++ src/routers/Users.ts
@@ -1,24 +1,65 @@
 import { Request, Response, Router, Express } from "express";
+import { getConnection } from "typeorm";
+import { Users } from "../entity/Users";
 
 // Init shared
 const router = Router();
 
-router.post("/register", (req: Request, res: Response) => {
+router.post("/register", async (req: Request, res: Response) => {
+  let data = req.body;
+  console.log(data)
+  if ("userName" in data && "email" in data && "age" in data) {
+    await getConnection()
+      .createQueryBuilder()
+      .insert()
+      .into(Users)
+      .values({
+        email: data.email,
+        age: data.age,
+        userName: data.userName,
+        userState: "outstanding Authozation",
+        gender: "unSelected"
+      })
+      .execute()
+      .then(() => {
+        res.status(401).send({
+          result: "success"
+        });
+        //send settPassword E-mail
+      });
+  } else {
+    res.status(401).send({
+      error: "invalid_request"
+    });
+  }
+});
+router.get("/availableID", async (req: Request, res: Response) => {
+  let ID = req.body.id;
+  let idCount = await getConnection()
+    .createQueryBuilder()
+    .select("userID")
+    .from(Users, "users")
+    .where("users.userID = :Id", { Id: ID })
+    .getCount();
+  res.status(200).send(
+    idCount == 0
+      ? {
+          result: "valiable"
+        }
+      : {
+          result: "unavailable"
+        }
+  );
+});
+router.post("/setPassword", async (req: Request, res: Response) => {
   res.status(401).send({
     error: "Not Defined"
   });
 });
 
-router.post("/setPassword", (req: Request, res: Response) => {
-  res.status(401).send({
-    error: "Not Defined"
-  });
-});
-
-router.post("/authorize", (req: Request, res: Response) => {
+router.post("/authorize", async (req: Request, res: Response) => {
   res.status(200).send({
-    result: "seuccess",
-    userName: "userName"
+    result: "failed"
   });
 });
 
