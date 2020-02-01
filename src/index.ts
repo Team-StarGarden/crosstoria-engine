0a347d4788f9db07f7f6ddc477b76d9f7f1f06e5
=piesignal <=leeon@insiro.me>
2020-02-02 01:25:09 +0900

    add: session

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, {json, Request, Response} from 'express';
 import BaseRouter from './routers';
 import {config} from './config';
-
+import session from 'express-session';
 import cors from 'cors';
 import {createConnection} from 'typeorm';
 import 'reflect-metadata';
@@ -15,27 +15,33 @@ const cornsOptions: cors.CorsOptions = {
 };
 
 export const connection = createConnection()
-.then(async connection => {
-  connection.synchronize();
-  const app = express();
-  app.use(cors(cornsOptions));
-  app.use(json());
-
-  app.get('/', (req: Request, res: Response) => {
-    res.send({
-      msg: 'Hello, World!',
+  .then(async connection => {
+    connection.synchronize();
+    const app = express();
+    app.use(cors(cornsOptions));
+    app.use(json());
+    app.use(
+      session({
+        secret: '@#!$^@$crosstoria#@$#$',
+        resave: false,
+        saveUninitialized: true,
+      }),
+    );
+    app.get('/', (req: Request, res: Response) => {
+      res.send({
+        msg: 'Hello, World!',
+      });
     });
-  });
 
-  app.use('*', cors(cornsOptions));
-  //set Router
-  app.use('/api', BaseRouter);
-  app.get('*', (req: Request, res: Response) => {
-    res.status(404).send({error: 'Not Found'});
-  });
+    app.use('*', cors(cornsOptions));
+    //set Router
+    app.use('/api', BaseRouter);
+    app.get('*', (req: Request, res: Response) => {
+      res.status(404).send({error: 'Not Found'});
+    });
 
-  app.listen(config.port, (): void => {
-    console.log('Crosstoria Engine is Listening...');
-  });
-})
-.catch(error => console.log(error));
+    app.listen(config.port, (): void => {
+      console.log('Crosstoria Engine is Listening...');
+    });
+  })
+  .catch(error => console.log(error));
